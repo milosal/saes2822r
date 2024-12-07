d70d6a89caa534e323eb6677214035d81d7b74cb
AnaisKillian <anaiskillian@college.harvard.edu>
2024-12-07 15:24:19 -0500

    renamed

--- datasets.py
+++ datasets.py
@@ -2,7 +2,7 @@ import pandas as pd
 import torch
 # WMDP at https://huggingface.co/datasets/cais/wmdp
 
-from datasetssssdfsdf import load_dataset
+from datasets import load_dataset
 
 ds_bio = load_dataset("cais/wmdp", "wmdp-bio")
 ds_chem = load_dataset("cais/wmdp", "wmdp-chem")
